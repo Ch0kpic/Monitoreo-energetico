4cf8604971ab4a396aa42940132591bfbd853f36
Cristobal Valenzuela Chavez <cristobalvalenzuelachavez@gmail.com>
2025-08-28 15:28:49 -0400

    Agregando modelo Categoria

--- dispositivos/models.py
+++ dispositivos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="dispositivos")
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE, related_name="dispositivos")
+    consumo_maximo = models.IntegerField(default=100)  # límite por defecto
