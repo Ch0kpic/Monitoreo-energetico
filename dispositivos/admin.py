db2dc61f2b55f86dfb36ba965160692ba8ce59b9
Ch0kpic <dylanjbs01@gmail.com>
2025-08-28 17:06:55 -0400

    urls.py

--- dispositivos/admin.py
+++ dispositivos/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Categoria, Zona, Dispositivo, Medicion, Alerta
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+
+@admin.register(Zona)
+class ZonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'marca', 'modelo', 'potencia_nominal', 'estado', 'categoria', 'zona')
+    list_filter = ('estado', 'categoria', 'zona')
+
+@admin.register(Medicion)
+class MedicionAdmin(admin.ModelAdmin):
+    list_display = ('dispositivo', 'fecha_hora', 'consumo')
+    list_filter = ('dispositivo',)
+
+@admin.register(Alerta)
+class AlertaAdmin(admin.ModelAdmin):
+    list_display = ('dispositivo', 'medicion', 'fecha_hora', 'tipo', 'mensaje')
+    list_filter = ('tipo', 'dispositivo')
+
